ac19dc06ee3dc5e077d529b90a01c4cbed50cbb8
therealpj24 <theshishir01@gmail.com>
2021-08-26 16:30:54 +0545

    virtual keyboard and test file added

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import cv2
-import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
 
-img = cv2.imread('test.png')
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+detector = HandDetector(detectionCon=0.7)
 
-edge = cv2.Canny(img_gray, 90, 10)
-nimg = img.copy()
-ret, thresh = cv2.threshold(edge, 150, 255, cv2.THRESH_BINARY)
+def Draw(img):
+    for i in buttonList:
+        img = i.draw(img)
+    return img
+
+class Button():
+    def __init__(self, pos, text, size=[100, 100]):
+        self.pos = pos
+        self.text = text
+        self.endPos = [size[0] + pos[0], size[1] + pos[1]]
+
+    def draw(self, img):
+        cv2.rectangle(img, self.pos, self.endPos, (255, 0, 255))
+        cv2.putText(img, self.text, (self.pos[0] + 20, self.pos[1] + 80), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+        return img
 
 
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-print(contours)
-# cv2.drawContours(image=img, contours=contours,contourIdx=-1, color=(0,255,0), thickness=8,lineType=cv2.LINE_AA)
-import random
+val = list('QWERTY')
+buttonQ = Button([100, 100], "Q")
+
+buttonList = []
+
+for j, i in enumerate(val):
+    cl = Button([(j + 1) * 100, 100], i)
+    buttonList += [cl]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist, bboxInfo = detector.findPosition(img)
+    img = Draw(img)
 
 
-for i, contour in enumerate(contours):  # loop over one contour area
-    for j, contour_point in enumerate(contour):  # loop over the points
-        b,g,r = random.randint(1,255),random.randint(1,255),random.randint(1,255)
-        # draw a circle on the current contour coordinate
-        cv2.circle(img, (contour_point[0][0], contour_point[0][1]), 2, (b,g,r), 2, cv2.LINE_AA)
+    if lmlist:
+        for button in buttonList:
+            x,y = button.pos
+            w,h = button.endPos
 
-cv2.imshow('Test', img)
+            if x<=lmlist[8][0]<=w and y<=lmlist[8][1]<=h:
+                cv2.rectangle(img, button.pos, button.endPos, (0, 0, 255), cv2.FILLED)
+                cv2.putText(img, button.text, (button.pos[0] + 20, button.pos[1] + 80), cv2.FONT_HERSHEY_PLAIN, 5,
+                            (255, 255, 255), 5)
 
-cv2.waitKey()
+
+    cv2.imshow('Image', img)
+
+    cv2.waitKey(1)
